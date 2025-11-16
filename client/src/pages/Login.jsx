389f568d977918dc288d756c5a26e285425d3e30
9326lalit <lalitkhairnar93@gmail.com>
2025-11-16 10:55:10 +0530

    c1

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -119,7 +119,7 @@ const Login = () => {
 
           <div className="mt-6 space-y-3 text-center">
             <p className="text-gray-600">
-              Don't have an account?{' '}
+              Don't havsdfe an account?{' '}
               <Link to="/register" className="text-blue-600 hover:text-blue-700 font-medium">
                 Create account
               </Link>
