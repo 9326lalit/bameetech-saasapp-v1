7dad741bd896fd6478ce03e2bb35f889bab71768
9326lalit <lalitkhairnar93@gmail.com>
2025-11-22 20:43:21 +0530

    updated

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,35 +1,3 @@
-// const { Sequelize } = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,     // database name
-//   process.env.DB_USER,     // username
-//   process.env.DB_PASSWORD, // password
-//   {
-//     host: process.env.DB_HOST,  
-//     dialect: process.env.DB_DIALECT || 'mysql',
-//     logging: false,
-//     dialectOptions: process.env.DB_SSL === 'true' ? {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
-//     } : {},
-//   }
-// );
-
-// const testConnection = async () => {
-//   try {
-//     await sequelize.authenticate();
-//     console.log('✅ Database connected successfully!');
-//   } catch (error) {
-//     console.error('❌ Unable to connect to the database:', error);
-//   }
-// };
-
-// module.exports = { sequelize, testConnection };
-
-
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
